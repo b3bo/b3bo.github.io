4722440722152ffa333be80959367455b4cd8930
Bebo <34556629+b3bo@users.noreply.github.com>
2025-07-29 11:28:53 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './*.html',          // Scans root HTML files (e.g., hero.html)
-    './components/breadcrumb/*.html', // Specific path to breadcrumb
+    './components/breadcrumb/breadcrumb.html', 
     './components/sierra/.si-site-container.html',
     './components/sierra/*.html',
     './components/*.html' // Scans other component files if added
