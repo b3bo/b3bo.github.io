45f380a913c763a175a94aa83acf039b9437e089
Bebo <34556629+b3bo@users.noreply.github.com>
2025-07-30 06:30:41 -0500

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
-    './*.html',          // Scans root HTML files (e.g., hero.html)
-    './components/breadcrumb/breadcrumb.html', 
+    
+    './components/breadcrumb/breadcrumb.html',  
     './components/sierra/.si-site-container.html',
-    './components/sierra/*.html',
+    './components/sierra/header.html',
+    './*.html',           // Scans root HTML files (e.g., hero.html)
     './components/*.html' // Scans other component files if added
   ],
   theme: {
