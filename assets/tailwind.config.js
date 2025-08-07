911a40ab3eef549bd061690fab2592a8a5ff74b2
Bebo <34556629+b3bo@users.noreply.github.com>
2025-08-06 19:41:51 -0500

    Update tailwind.config.js

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/breadcrumb.html",
+    "C:\Users\johnb\Documents\GitHub\b3bo.github.io\componentsbreadcrumb.html",
     "./components/faq.html",
     "./components/**/*.html",
     "./assets/**/*.css"
