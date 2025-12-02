ddf7f4b2dad06044a749bdb536cd9413b36bf17b
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-02 08:20:01 -0600

    Add cache-busting to utils.js import

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 import { CONFIG } from './config.js';
 import { STATE } from './state.js';
-import { getUrlParams, toSlug } from './utils.js';
+import { getUrlParams, toSlug } from './utils.js?v=202501';
 import { initializeMap, computeOffsetPx, offsetLatLng, fitBoundsToNeighborhoods } from './map.js'; // computeOffsetPx needed for single mode
 import { loadNeighborhoods } from './data.js';
 import { setupUI, navigateNeighborhood } from './ui.js';
