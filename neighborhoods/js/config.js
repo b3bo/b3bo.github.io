3c2c7745bdf7ae7a0eb7ae49c88a4bcb73f28300
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-08 08:41:11 -0600

    Update config.js

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -51,8 +51,8 @@ export const CONFIG = {
 
         // Paths to neighborhood JSON data files (base64 encoded)
         neighborhoodFiles: [
-            '././data/7ea1bf14d884d192.json',
-            '././data/d2ea7fdfc87ff3e7.json'
+            './data/7ea1bf14d884d192.json',
+            './data/d2ea7fdfc87ff3e7.json'
         ]
     },
 
