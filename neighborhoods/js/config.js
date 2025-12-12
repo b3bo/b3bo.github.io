1c029267bff01c0f2a9b682b1964bf4500328ca1
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-12 13:43:15 -0600

    add search links

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.39',
+    version: '1.0.40',
 
     // ===================================================================
     // MAP SETTINGS
