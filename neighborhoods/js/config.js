4fef3f31ea2406a9877b6f7393c24a7290c4a209
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-12 18:01:27 -0600

    add docks

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.41',
+    version: '1.0.42',
 
     // ===================================================================
     // MAP SETTINGS
