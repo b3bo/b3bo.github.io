de196b866b2666f927ae6f12f279cf303add6f0d
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-19 12:41:48 -0600

    overlay

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.12',
+    version: '1.0.13',
 
     // ===================================================================
     // MAP SETTINGS
