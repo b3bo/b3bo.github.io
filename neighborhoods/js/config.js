42cf872990cd7793c91d7ace51fd2f3f0a09cb8a
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-20 12:40:44 -0600

    restoration a93af394c5a7ea10a164aff38cbab5f6ec98f726

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.23',
+    version: '1.0.24',
 
     // ===================================================================
     // MAP SETTINGS
