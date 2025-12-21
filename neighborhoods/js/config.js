f78c1391bf01ca22a1b670692035790b7aebfa4a
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-21 06:34:39 -0600

    fix fade

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.31',
+    version: '1.0.32',
 
     // ===================================================================
     // MAP SETTINGS
