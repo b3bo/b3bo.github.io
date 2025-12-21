4a9cbd199ab62e203638e8b35ba9cc24da7e652c
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-21 15:53:32 -0600

    update Home listings

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.0.35',
+    version: '1.0.36',
 
     // ===================================================================
     // MAP SETTINGS
