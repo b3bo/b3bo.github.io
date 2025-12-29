bd6fb6f8f9ab720c6b0ee43f95a9a0a67ab6ceaf
Bebo <34556629+b3bo@users.noreply.github.com>
2025-12-29 16:52:25 -0600

    slow down animation

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.1.15',
+    version: '1.1.16',
 
     // ===================================================================
     // MAP SETTINGS
