05d13de7f6b17f40677f35a58d990e97ed2722c7
Bebo <34556629+b3bo@users.noreply.github.com>
2026-01-02 11:59:31 -0600

    fix for iframe

--- neighborhoods/js/config.js
+++ neighborhoods/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.1.33',
+    version: '1.1.34',
 
     // ===================================================================
     // MAP SETTINGS
