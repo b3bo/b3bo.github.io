60543870b8abaaf5b14bbbdf10a11b5858851784
Bebo <34556629+b3bo@users.noreply.github.com>
2026-01-13 21:46:20 -0600

    Fix home icon color override with inline style

--- assets/websites/truesouthcoastalhomes/sierra/js/nav-cards.js
+++ assets/websites/truesouthcoastalhomes/sierra/js/nav-cards.js
@@ -81,7 +81,7 @@
     var breadcrumbHtml = [
       '<nav aria-label="Breadcrumb" class="mt-2 mb-4">',
       '<div class="breadcrumb-items flex justify-center items-center px-4 space-x-2 md:space-x-4 font-body text-neutral-700">',
-      '<a href="/" class="text-primary-600 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500"><i class="fa fa-home"></i></a>',
+      '<a href="/" class="text-primary-600 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500" style="color: var(--color-primary-600);"><i class="fa fa-home"></i></a>',
       '<svg class="w-4 h-4 text-neutral-400 flex-shrink-0" style="width: 1rem; height: 1rem;" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">',
       '<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>',
       '</svg>',
