c9579d68eb1445bb8cb4988e3c3f4df59ab15a0f
Bebo <34556629+b3bo@users.noreply.github.com>
2026-03-08 07:43:30 -0500

    Update neighborhoods v1.2.23

--- assets/js/config.js
+++ assets/js/config.js
@@ -17,7 +17,7 @@
  */
 export const CONFIG = {
     // App version - MAJOR.MINOR.PATCH (auto-incremented on deploy)
-    version: '1.2.22',
+    version: '1.2.23',
 
     // ===================================================================
     // MAP SETTINGS
